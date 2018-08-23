1f8bd6b6b6bf7f38d89812ae93a323aecede3722
Kaak99 <cours.kaak@gmail.com>
2018-08-23 17:35:36 +0200

    essai methode conv objet Humeur en json

--- app/src/main/java/com/example/kaak/projet030/model/Humeur.java
+++ app/src/main/java/com/example/kaak/projet030/model/Humeur.java
@@ -2,28 +2,22 @@ package com.example.kaak.projet030.model;
 
 public class Humeur {
     private int laDate;
-
-
-
-    public int getHumeurDuJour() {
-        return humeurDuJour;
-    }
-
-    public void setHumeurDuJour(int humeurDuJour) {
-        this.humeurDuJour = humeurDuJour;
-    }
-
     private int humeurDuJour;
-
-
-
-    public String getCommentaireDuJour() {
-        return commentaireDuJour;
-    }
-
-    public void setCommentaireDuJour(String commentaireDuJour) {
-        this.commentaireDuJour = commentaireDuJour;
-    }
-
     private String commentaireDuJour;
+
+    public Humeur(laDate,humeurDuJour,commentaireDuJour) {
+        this.laDate=laDate;
+        this.humeurDuJour=humeurDuJour;
+        this.commentaireDuJour=commentaireDuJour;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Humeur [ " +
+                "date='" + laDate + '\'' +
+                ", humeur du jour='" + humeurDuJour + '\'' +
+                ", commentaire du jour='" + commentaireDuJour + '\'' +
+                ']';
+    }
 }
