19a6958a53b7e238b86abf544062b4545e7e7718
Kaak99 <cours.kaak@gmail.com>
2018-08-23 17:20:24 +0200

    ajout gson, creation classe modele Humeur

--- app/src/main/java/com/example/kaak/projet030/model/Humeur.java
+++ app/src/main/java/com/example/kaak/projet030/model/Humeur.java
@@ -1,4 +1,29 @@
 package com.example.kaak.projet030.model;
 
 public class Humeur {
+    private int laDate;
+
+
+
+    public int getHumeurDuJour() {
+        return humeurDuJour;
+    }
+
+    public void setHumeurDuJour(int humeurDuJour) {
+        this.humeurDuJour = humeurDuJour;
+    }
+
+    private int humeurDuJour;
+
+
+
+    public String getCommentaireDuJour() {
+        return commentaireDuJour;
+    }
+
+    public void setCommentaireDuJour(String commentaireDuJour) {
+        this.commentaireDuJour = commentaireDuJour;
+    }
+
+    private String commentaireDuJour;
 }
